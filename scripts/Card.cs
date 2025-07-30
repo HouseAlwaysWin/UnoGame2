1346d65c1012dcfa71df1b483f8e9e7dbc2820d2
MartinWang <martinwang7963@gmail.com>
2025-07-30 20:58:25 +0800

    update button color

---
+++ scripts/Card.cs
@@ -0,0 +1,129 @@
+using Godot;
+using System;
+
+public enum CardType
+{
+    Number,
+    Skip,
+    Reverse,
+    DrawTwo,
+    Wild,
+    WildDrawFour
+}
+
+public enum CardColor
+{
+    Red,
+    Blue,
+    Green,
+    Yellow
+}
+
+public partial class Card : TextureRect
+{
+    [Export]
+    public CardColor Color { get; set; } = CardColor.Red;
+    
+    [Export]
+    public string CardValue { get; set; } = "0";
+    
+    [Export]
+    public CardType Type { get; set; } = CardType.Number;
+    
+    public override void _Ready()
+    {
+        // 初始化卡片顯示
+        UpdateCardDisplay();
+    }
+    
+    public void SetCard(CardColor color, string value, CardType type = CardType.Number)
+    {
+        Color = color;
+        CardValue = value;
+        Type = type;
+        
+        UpdateCardDisplay();
+    }
+    
+    private void UpdateCardDisplay()
+    {
+        // 根據卡片屬性設置紋理
+        string texturePath = GetCardTexturePath();
+        if (ResourceLoader.Exists(texturePath))
+        {
+            Texture2D texture = ResourceLoader.Load<Texture2D>(texturePath);
+            if (texture != null)
+            {
+                Texture = texture;
+            }
+        }
+    }
+    
+    private string GetCardTexturePath()
+    {
+        string basePath = "res://assets/";
+        string colorString = Color.ToString().ToLower();
+        
+        switch (Type)
+        {
+            case CardType.Skip:
+                return basePath + colorString + "Skip.png";
+            case CardType.Reverse:
+                return basePath + colorString + "Reverse.png";
+            case CardType.DrawTwo:
+                return basePath + colorString + "DrawTwo.png";
+            case CardType.Wild:
+                return basePath + "wild.png";
+            case CardType.WildDrawFour:
+                return basePath + "wildDrawFour.png";
+            default: // CardType.Number
+                return basePath + colorString + CardValue + ".png";
+        }
+    }
+    
+    public void SetCardBack()
+    {
+        // 設置卡片背面
+        string backPath = "res://assets/back.png";
+        if (ResourceLoader.Exists(backPath))
+        {
+            Texture2D backTexture = ResourceLoader.Load<Texture2D>(backPath);
+            if (backTexture != null)
+            {
+                Texture = backTexture;
+            }
+        }
+    }
+    
+    public void SetCardFront()
+    {
+        // 顯示卡片正面
+        UpdateCardDisplay();
+    }
+    
+    public bool CanPlayOn(Card topCard)
+    {
+        // 檢查是否可以打出這張牌
+        if (Type == CardType.Wild || Type == CardType.WildDrawFour)
+        {
+            return true; // 萬能牌可以隨時打出
+        }
+        
+        // 檢查顏色或數字是否匹配
+        return Color == topCard.Color || CardValue == topCard.CardValue;
+    }
+    
+    public override void _GuiInput(InputEvent @event)
+    {
+        // 處理卡片點擊事件
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+        {
+            if (mouseEvent.ButtonIndex == MouseButton.Left)
+            {
+                GD.Print($"卡片被點擊: {Color} {CardValue}");
+                // 這裡可以發出自定義信號
+                EmitSignal("CardClicked", this);
+            }
+        }
+    }
+} 
